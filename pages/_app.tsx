16f6450efe0451bdf467fcaf9f491c9289e91825
Nawed Ali <nawed2611@gmail.com>
2022-11-20 05:01:08 +0530

    Add auth0 config for auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+// pages/_app.js
+import "../styles/globals.css";
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }: any) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
+import React, { useState } from 'react';
+import { useUser } from '@auth0/nextjs-auth0';
+import { useRouter } from 'next/router';
+import Link from "next/link"
 
 export default function Home() {
+  const { user } = useUser();
+  const router = useRouter();
+
+  if (user) {
+    router.push('/search')
+  }
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1 className="font-bold text-5xl">Welcome to ReSearch!</h1>
+    <div className="flex flex-col overflow-hidden h-screen items-center justify-center">
+      <h1 className="font-bold text-5xl">ReSearch!</h1>
+      <p className="mt-4 font-extralight text-xl">Exploring your study papers has never been easier!</p>
+      <Link href='/api/auth/login' className='p-2 text-white text-sm text-center rounded-lg hover:bg-green-700 cursor-pointer bg-green-600 my-8 px-4 hover:scale-105'>Lets Get Started</Link>
     </div>
   )
 }
