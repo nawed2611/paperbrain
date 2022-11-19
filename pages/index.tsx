bfae53255a085f1b4f4f49f150a2570a68729d3d
Nawed Ali <nawed2611@gmail.com>
2022-11-20 04:28:00 +0530

    Init client w/ Next.js TailwindCSS

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="font-bold text-5xl">Welcome to ReSearch!</h1>
+    </div>
+  )
+}
