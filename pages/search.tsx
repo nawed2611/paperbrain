543c385997d9fcae8ad2dea9558ec079097cfd4f
nawed2611 <nawed2611@gmail.com>
2023-01-11 19:18:14 +0530

    Add UI changes

--- pages/search.tsx
+++ pages/search.tsx
@@ -4,6 +4,7 @@ import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import Image from 'next/image';
+import { AiOutlineSearch } from 'react-icons/ai';
 import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
 
 import Layout from './layout';
@@ -25,18 +26,18 @@ const Search = () => {
           initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
-          className='flex flex-col items-center p-6 w-[60vw]'>
+          className='flex flex-col items-center p-12'>
 
           <RoughNotation animationDelay={1000} animationDuration={2000} type="highlight" color='#fff000' show={true}>
             <div className='p-8'>
               <h1 className='text-4xl text-gray-900 font-bold'>Looking for Papers?</h1>
-              <h1 className='text-2xl text-gray-500 font-bold'>We have got you covered.</h1>
+              <h1 className='text-2xl text-gray-500 font-bold'>We've got you covered.</h1>
             </div>
           </RoughNotation>
 
-          <form onSubmit={handleSubmit} className="flex flex-col items-center justify-center p-2">
-            <input type="text" className="bg-white mt-2 text-green-600 h-12 w-[30vw] px-6 rounded-full border-2 focus:outline-none " value={query} onChange={(e) => setQuery(e.target.value)} placeholder="Enter your Search" />
-            <button className="border m-3 border-black py-1 w-20 hover:scale-105 transition-all text-sm rounded-full  hover:bg-slate-700 hover:text-slate-50" type='submit'>Search</button>
+          <form onSubmit={handleSubmit} className="flex rounded-full border-2 p-1 mt-6 items-center justify-center">
+            <input type="text" className="bg-white mt-2 text-green-600 focus:outline-none px-4 w-[24vw]" value={query} onChange={(e) => setQuery(e.target.value)} placeholder="eg: GPT-3 Stable Diffusion etc..." />
+            <button className="flex items-center hover:scale-105 p-2 transition-all rounded-full  hover:bg-slate-700 hover:text-slate-50" type='submit'><AiOutlineSearch size={21} /></button>
           </form>
         </motion.div>
 
@@ -46,17 +47,17 @@ const Search = () => {
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5 }}
             className="absolute top-4 right-12 py-8 px-8 mx-auto bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
-            <Image blurDataURL={`${user.picture}`} src={`${user.picture}`} alt="user-profile-picture" className="rounded-full w-16 h-16" width={16} height={16} />
-            <div className="text-center space-y-2 sm:text-left">
+            <Image src={`${user.picture}`} alt="user-profile-picture" className="rounded-full w-16 h-16" width={16} height={16} />
+            <div className="text-center flex flex-col items-center space-y-2 sm:text-left">
               <div className="space-y-0.5 mb-2">
-                <p className="text- text-black font-semibold">
+                <p className=" text-black font-semibold">
                   {user.name}
                 </p>
                 <p className="text-slate-500 font-medium">
                   {user.email}
                 </p>
               </div>
-              <Link scroll={false} href='/api/auth/logout' className="px-4 py-1 transition-all text-sm text-green-600 font-semibold rounded-full border border-green-200 hover:text-white hover:bg-green-400 hover:border-transparent focus:outline-none focus-2 focus-green-600 focus-offset-2">
+              <Link scroll={false} href='/api/auth/logout' className="px-4 py-1 text-center transition-all text-sm text-green-600 font-semibold rounded-full border border-green-200 hover:text-white hover:bg-green-400 hover:border-transparent focus:outline-none focus-2 focus-green-600 focus-offset-2">
                 Logout
               </Link>
             </div>
--- pages/search/[slug].tsx
+++ pages/search/[slug].tsx
@@ -3,7 +3,11 @@ import { client } from '../../utils/client';
 import { motion } from 'framer-motion'
 import { useRouter } from 'next/router';
 import Link from 'next/link';
+import Image from 'next/image';
 import Layout from '../layout';
+import { useUser } from '@auth0/nextjs-auth0';
+import { IoIosArrowDown, IoIosMoon } from 'react-icons/io';
+import { AiOutlineUser, AiOutlineLogout, AiOutlineSearch } from 'react-icons/ai';
 import { toast, Toaster } from 'react-hot-toast';
 import { RoughNotation } from "react-rough-notation";
 
@@ -11,9 +15,12 @@ const SearchResults = () => {
     const router = useRouter();
     const [loading, setLoading] = useState(false);
     const { slug } = router.query;
+    const [newQuery, setNewQuery] = useState('')
+    const { user } = useUser();
 
     const [pdfs, setPdfs] = useState('');
     const [modal, setOpenModal] = useState(false);
+    const [userProfileMenu, setUserProfileMenu] = useState(false);
     const [modalContent, setModalContent] = useState({
         paper_title: '',
         paper_summary: '',
@@ -30,7 +37,6 @@ const SearchResults = () => {
     }
 
     const handleClick = (e: any) => {
-
         router.push(`/pdf/${e.paper_title.replace(/ /g, '-')}`);
     }
 
@@ -49,30 +55,40 @@ const SearchResults = () => {
             })
     }, [slug]);
 
+    const handleUserProfileClick = () => {
+        setUserProfileMenu(!userProfileMenu);
+    }
+
+    const handleSubmit = (e: any) => {
+        e.preventDefault();
+        router.push(`/search/${newQuery}`);
+    }
+
     return (
         <Layout className='overflow-hidden'>
             <div className='flex'>
                 <Toaster />
                 {
                     response &&
-                    <div className='border border-black-2'>
-                        <div className='p-3 w-[30vw] flex flex-col items-center justify-center'>
-                            <h2 className='font-bold text-2xl capitalize m-2'>Search Results for: {slug}</h2>
+                    <div className='border-2 border-green-100'>
+                        <div className='p-3 flex flex-col gap-y-2 items-center justify-center'>
+                            <h2 className='font-bold text-xl capitalize m-2'>Papers for: {slug}</h2>
                             <button className="border rounded border-black p-1 px-4 hover:bg-gray-50 transition-all hover:scale-105">
                                 <Link href="/search">Go Back to Search</Link>
                             </button>
                         </div>
-                        <div className='h-[83vh] w-[30vw] overflow-y-scroll border-2 flex flex-col items-center'>
+                        <div className='h-[83vh] w-[33vw] m-2 overflow-x-hidden scrollbarHide flex flex-col items-center'>
                             {
                                 response.map((paper: any) => {
                                     return (
-                                        <div key={paper.paper_title} className='w-full p-2 flex flex-col items-center justify-center border-b border-black-2'>
+                                        <div key={paper.paper_title} className='w-full border-2 border-green-200 m-2 flex flex-col items-center justify-center'>
                                             <div className='flex flex-col items-center justify-center'>
                                                 <div className='flex flex-wrap flex-col items-center justify-center'>
-                                                    <h3 className='font-bold text-xl p-3 bg-green-50'>{paper.paper_title}</h3>
-                                                    <p className='text-sm text-clip overflow-hidden w-96 h-32 p-2'>{paper.paper_summary}</p>
+                                                    <h3 className='font-bold text-xl p-4 bg-green-50'>{paper.paper_title}</h3>
+                                                    <p className='text-sm text-clip overflow-hidden h-32 p-4'>{paper.paper_summary}</p>
                                                 </div>
-                                                <button onClick={() => openModal(paper)} className='border hover:scale-105 transition-all border-black rounded-md p-2 mt-6 mb-2 w-32'>Read More</button>
+                                                <button onClick={() => openModal(paper)} className='border text-sm hover:scale-105 transition-all border-black rounded-md p-2 mt-6 mb-2'>Continue Reading
+                                                </button>
                                             </div>
                                         </div>
                                     )
@@ -81,13 +97,59 @@ const SearchResults = () => {
                             {
                                 loading &&
                                 <div className='flex flex-col items-center justify-center'>
-                                    <h1 className='font-bold text-2xl'>Loading...</h1>
+                                    <p className='font-bold text-sm'>Loading...</p>
                                 </div>
                             }
                         </div>
                     </div>
                 }
-                <div className='w-full h-[98vh] overflow-y-scroll flex items-center'>
+                <div className='w-full h-[98vh] flex flex-col overflow-y-scroll items-center'>
+                    {
+                        user &&
+                        <motion.div
+                            initial={{ opacity: 0, scale: 0.5 }}
+                            animate={{ opacity: 1, scale: 1 }}
+                            transition={{ duration: 0.5 }}
+                            className="border-b border-green-200 w-full flex items-center justify-between">
+
+                            <form onSubmit={handleSubmit} className="flex rounded-full border-2 p-1 ml-12 items-center justify-center">
+                                <input type="text" className="bg-white mt-2 text-green-600 focus:outline-none px-4 w-[40vw]" value={newQuery} onChange={(e) => setNewQuery(e.target.value)} placeholder="eg: GPT-3 Stable Diffusion etc..." />
+                                <button className="flex items-center hover:scale-105 p-2 transition-all rounded-full  hover:bg-slate-700 hover:text-slate-50" type='submit'><AiOutlineSearch size={21} /></button>
+                            </form>
+                            {/*                             
+                            <button className="hover:bg-gray-50 mx-2 rounded-full p-2 border transition-all hover:scale-105">
+                                <IoIosMoon />
+                            </button> */}
+                            <div className="flex m-2  p-2 items-center justify-center">
+                                <Image src={`${user.picture}`} alt="user-profile-picture" className="rounded-full" width={32} height={32} />
+                                <div className="items-center flex p-2 sm:text-left">
+                                    <p className="text-center font-semibold">
+                                        {user.name}
+                                    </p>
+                                    {/* add an svg for a dropdown menu */}
+                                    <button onClick={handleUserProfileClick} className=" hover:bg-gray-50 mx-2 rounded-full p-2 border transition-all hover:scale-105">
+                                        <IoIosArrowDown />
+                                    </button>
+                                    {
+                                        userProfileMenu &&
+                                        <motion.div
+                                            initial={{ opacity: 0, scale: 0.5 }}
+                                            animate={{ opacity: 1, scale: 1 }}
+                                            transition={{ duration: 0.2 }}
+                                            className="absolute flex flex-col top-14 right-12 mt-2 bg-white rounded-md shadow-xl py-1">
+                                            <Link className='flex items-center hover:bg-gray-50 m-1 p-2 px-4 text-sm text-center' href="/profile">
+                                                <AiOutlineUser className='mr-2' />Profile
+                                            </Link>
+                                            <Link className='flex items-center hover:bg-gray-50 m-1 p-2 px-4 text-sm text-center' href="/api/auth/logout">
+                                                <AiOutlineLogout className='mr-2' />Logout
+                                            </Link>
+                                        </motion.div>
+
+                                    }
+                                </div>
+                            </div>
+                        </motion.div>
+                    }
                     {
                         modal &&
                         <motion.div
@@ -96,17 +158,18 @@ const SearchResults = () => {
                             transition={{ duration: 0.5 }}
                             className='flex flex-col justify-center items-center m-4'>
                             <div className='flex bg-slate-100 glass rounded-md flex-col items-center justify-center p-8 border border-black'>
-                                <RoughNotation animationDelay={1000} animationDuration={2000} type="highlight" color='#fff000' show={modal}>
+                                <RoughNotation animationDelay={1000} animationDuration={2000} type="highlight" color='#cef2c4' show={modal}>
                                     <h1 className='font-bold text-3xl p-2 m-2'>{modalContent?.paper_title}</h1>
                                 </RoughNotation>
-                                <p className='w-[90%] font-bold text-2xl mt-12'>Abstract  {' '}</p>
-                                <p className='w-[90%] text-lg mt-2'>{modalContent?.paper_summary}</p>
+                                <p className='w-[90%] font-bold underline text-xl mt-12'>Abstract</p>
+                                <p className='w-[90%] text-base mt-2'>{modalContent?.paper_summary}</p>
                                 <div>
                                     <button onClick={() => handleClick(modalContent)} className='border hover:scale-105 transition-all border-black hover:bg-slate-100 rounded-md p-1 mt-6 mb-2 w-32'>View PDF</button>
                                 </div>
                             </div>
                         </motion.div>
                     }
+
                 </div>
             </div>
         </Layout>
