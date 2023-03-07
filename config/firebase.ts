597b11925935e80e152e09cf1f644400245a1c3e
nawed2611 <nawed2611@gmail.com>
2023-03-07 18:09:09 +0530

    Add config to env

--- config/firebase.ts
+++ config/firebase.ts
@@ -7,13 +7,13 @@ import { getStorage } from "firebase/storage";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDO8L77QX3bq7tf5GqOFOvXkkqq0a3h63c",
-  authDomain: "legal-ai-8ebe8.firebaseapp.com",
-  projectId: "legal-ai-8ebe8",
-  storageBucket: "legal-ai-8ebe8.appspot.com",
-  messagingSenderId: "190575471301",
-  appId: "1:190575471301:web:26c3b521bed6c4fd889eac",
-  measurementId: "G-E8VNF009MP",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
