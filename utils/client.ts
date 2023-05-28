9ca7992c63dadb5515fc71c089b3bc5c182ac459
Md Anzar Ahmad <anzarhps@gmail.com>
2023-05-29 00:24:01 +0530

    Update backend url

--- utils/client.ts
+++ utils/client.ts
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookies from 'js-cookie';
 
 export const client = axios.create({
-  baseURL: 'https://paperbrain.onrender.com/',
+  // baseURL: 'https://paperbrain.onrender.com/',
+  baseURL: 'https://web-production-f9ae.up.railway.app/',
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${Cookies.get('apiKey')}`,
